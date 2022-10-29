67b927a198ee617be9917002f6734f24cf842736
Tom Holmes <thg.holmes96@gmail.com>
2022-10-29 15:42:29 +0100

    Initial Commit

---
+++ ASCII Photo/ASCII_PhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCII_PhotoApp.swift
+//  ASCII Photo
+//
+//  Created by Tom Holmes on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ASCII_PhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
