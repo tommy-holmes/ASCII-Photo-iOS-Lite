0f9ad37c0ec177b6e9146341ed80e9ec52b45727
Tom Holmes <thg.holmes96@gmail.com>
2022-12-11 16:15:21 +0000

    Adds test bundle

---
+++ ASCII PhotoTests/ASCII_PhotoTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+@testable import ASCII_Photo
+
+final class ASCII_PhotoTests: XCTestCase {
+    
+    private let model = ImageModel()
+
+    func testUpdateImage() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
